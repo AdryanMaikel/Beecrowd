3aa50e465c8d5819865081ac0fc7f4db75289baf
AdryanMaikel <adryanmaikel@gmail.com>
2024-01-12 17:52:51 -0300

    calculando a area do circulo

---
+++ Iniciante/1002 - Área do Círculo/main.py
@@ -0,0 +1,3 @@
+RAIO, PI = float(input()), 3.14159
+AREA = RAIO * RAIO * PI
+print(f'A={AREA:.4f}')
---
+++ Iniciante/1002 - Área do Círculo/main.rb
@@ -0,0 +1,4 @@
+RAIO = gets.to_f.round(2)
+PI = 3.14159
+AREA = RAIO * RAIO * PI
+puts "A=#{format("%.4f", AREA)}"
